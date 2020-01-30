efc7caec76ba5e51b1df80c559de3137442a2492
kongzecheng <kzccat.nana@gmail.com>
2020-01-30 18:50:13 +0900

    1.2practice

---
+++ firstc.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+	std::cout << "shu ru liang ge shu:";
+	std::cout << std::endl;
+	int v1 = 0;
+	int v2 = 0;
+	std::cin >> v1;
+	std::cin >> v2;
+	std::cout << v1;
+	std::cout << "he";
+	std::cout << v2;
+	std::cout << "de cheng ji shi";
+	std::cout << v1 * v2;
+	std::cout << std::endl;
+}
